ebd1bf00509c2b9ba09bb0aaaf36819d045b1ed5
Matthias Nott <matthias.nott@sap.com>
2017-09-19 21:27:45 +0200

    Outsource business functionality to TDatasets
    Simplified datasets.pl by adding most of its functionality to a
    TDatasets bean.
    
    Signed-off-by: Matthias Nott <matthias.nott@sap.com>

--- html/datasets.pl
+++ html/datasets.pl
@@ -19,6 +19,10 @@ use File::Basename;
 use Apache2::compat;
 use Apache2::Request;
 
+use Data::Dump "pp";
+
+use CSVdb::TUtils qw (t_case);
+use CSVdb::TDatasets;
 
 ###################################################
 #
@@ -34,90 +38,50 @@ use Apache2::Request;
 #
 ###################################################
 
-my $path = "$ENV{ROOT}/data";
+my $req = Apache2::Request->new(shift);
 
-my $req = shift;
+my $ds = CSVdb::TDatasets->new( req => $req, );
 
-$req = Apache2::Request->new( $req );
+my $datasets = $ds->datasets;
 
-my @sub_dirs = grep { -d } map { catfile $path, $_ } read_dir $path;
+print <<EOF;
+Content-type: text/html
 
-my %datasets;
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+<div id="content" align="left">
+<table cellpadding="5" cellspacing="0" border="0" bordercolor="black" width="100%">
+<tr class="h"><td>&nbsp;</td><td class="l">Datasets</td></tr>
+EOF
 
-foreach my $sub_dir (@sub_dirs) {
-    my $location    = fileparse($sub_dir);
-    my $description = $location;
-    $description =~ s/_/ /g;
-    $description =~ s/([\w']+)/\u\L$1/g
-    $datasets{$location} = $description;
+for my $dataset ( @$datasets ) {
+    my $description = $ds->describe($dataset);
+
+    print <<EOF
+    <tr><td>&nbsp;</td></td><td><a href="/index.pl?dataset=$dataset" target="_top">$description</a></td>
+EOF
 }
 
-print STDERR $datasets{$_}, "\n" for sort keys %datasets;
+print <<EOF
+</table>
+</div>
+
+    <script>
+    function resizeIframe(iframeID) {
+        var iframe = window.parent.document.getElementById(iframeID);
+        var container = document.getElementById('content');
+        iframe.style.height = container.offsetHeight + 'px';
+    }
+    resizeIframe("datasets");
+    </script>
+</body>
+</html>
+EOF
 
 
 
-sub title_case {
-my @small_words = qw( (?<!q&)a an and as at(?!&t) but by en for if in of on or the to v[.]? via vs[.]? );
-my $small_re = join '|', @small_words;
-
-my $apos = qr/ (?: ['’] [[:lower:]]* )? /x;
-
-while ( <> ) {
-    s{\A\s+}{}, s{\s+\z}{};
-    $_ = lc $_ if not /[[:lower:]]/;
-    s{
-        \b (_*) (?:
-            ( (?<=[ ][/\\]) [[:alpha:]]+ [-_[:alpha:]/\\]+ |   # file path or
-              [-_[:alpha:]]+ [@.:] [-_[:alpha:]@.:/]+ $apos )  # URL, domain, or email
-            |
-            ( (?i: $small_re ) $apos )                         # or small word (case-insensitive)
-            |
-            ( [[:alpha:]] [[:lower:]'’()\[\]{}]* $apos )       # or word w/o internal caps
-            |
-            ( [[:alpha:]] [[:alpha:]'’()\[\]{}]* $apos )       # or some other word
-        ) (_*) \b
-    }{
-        $1 . (
-          defined $2 ? $2         # preserve URL, domain, or email
-        : defined $3 ? "\L$3"     # lowercase small word
-        : defined $4 ? "\u\L$4"   # capitalize word w/o internal caps
-        : $5                      # preserve other kinds of word
-        ) . $6
-    }xeg;
 
 
-    # Exceptions for small words: capitalize at start and end of title
-    s{
-        (  \A [[:punct:]]*         # start of title...
-        |  [:.;?!][ ]+             # or of subsentence...
-        |  [ ]['"“‘(\[][ ]*     )  # or of inserted subphrase...
-        ( $small_re ) \b           # ... followed by small word
-    }{$1\u\L$2}xig;
-
-    s{
-        \b ( $small_re )      # small word...
-        (?= [[:punct:]]* \Z   # ... at the end of the title...
-        |   ['"’”)\]] [ ] )   # ... or of an inserted subphrase?
-    }{\u\L$1}xig;
-
-    # Exceptions for small words in hyphenated compound words
-    ## e.g. "in-flight" -> In-Flight
-    s{
-        \b
-        (?<! -)                 # Negative lookbehind for a hyphen; we don't want to match man-in-the-middle but do want (in-flight)
-        ( $small_re )
-        (?= -[[:alpha:]]+)      # lookahead for "-someword"
-    }{\u\L$1}xig;
-
-    ## # e.g. "Stand-in" -> "Stand-In" (Stand is already capped at this point)
-    s{
-        \b
-        (?<!…)                  # Negative lookbehind for a hyphen; we don't want to match man-in-the-middle but do want (stand-in)
-        ( [[:alpha:]]+- )       # $1 = first word and hyphen, should already be properly capped
-        ( $small_re )           # ... followed by small word
-        (?! - )                 # Negative lookahead for another '-'
-    }{$1\u$2}xig;
-
-    print "$_";
-}
-}
---
+++ lib/CSVdb/TDatasets.pm
@@ -0,0 +1,148 @@
+package CSVdb::TDatasets;
+
+=pod
+
+=head1 NAME
+
+CSVdb::TDatasets - Wrapper for Datasets
+
+=head1 DESCRIPTION
+
+This class provides for a wrapper around the datasets.
+
+=head1 METHODS
+
+=cut
+
+use warnings;
+use strict;
+
+binmode STDOUT, ":utf8";
+use utf8;
+
+use File::Slurp qw( read_dir );
+use File::Spec::Functions qw( catfile );
+use File::Basename;
+
+use CSVdb::TSession;
+use CSVdb::TCache;
+use CSVdb::TUtils qw (t_case);
+
+use Data::Dump "pp";
+
+use Moose;
+with 'MooseX::Log::Log4perl';
+
+use namespace::autoclean -except => sub { $_ =~ m{^t_.*} };
+
+
+#
+# Properties
+#
+has req      => ( is => 'rw' );    # HTTP request
+has ses      => ( is => 'rw' );    # HTTP session
+has cache    => ( is => 'rw' );    # The cache
+has datasets => ( is => 'rw' );    # The datasets array
+has dataset  => ( is => 'rw' );    # The chosen dataset
+
+
+#
+# Constructor
+#
+sub BUILD {
+    my ( $self, $arg_ref ) = @_;
+
+    #
+    # Initialize the Logger
+    #
+    Log::Log4perl->init_once( $ENV{ROOT} . '/log4p.ini' );
+
+    my $log = Log::Log4perl::get_logger("CSVdb");
+
+    if ( exists $ENV{LOGLEVEL} && "" ne $ENV{LOGLEVEL} ) {
+        $log->level( uc $ENV{LOGLEVEL} );
+    }
+
+    #
+    # Initialize the cache
+    #
+    $self->cache( CSVdb::TCache->new );
+
+    #
+    # Initialize the HTTP session
+    #
+    $self->ses( CSVdb::TSession->new( req => $self->req ) );
+
+    #
+    # Read the list of datasets
+    #
+    my $datasets = $self->cache->get(
+        "datasets",
+        sub {
+            $self->log->debug("+ Reading datasets from file system");
+
+            my $path = "$ENV{ROOT}/data";
+
+            my @sub_dirs = grep {-d} map { catfile $path, $_ } read_dir $path;
+
+            my @datasets;
+
+            foreach my $sub_dir (@sub_dirs) {
+                my $location = fileparse($sub_dir);
+                push( @datasets, $location );
+            }
+
+            return \@datasets;
+        }
+    );
+    $self->datasets($datasets);
+
+
+    #
+    # Try to read the data set from the parameter
+    #
+    my $dataset = $self->req->param("dataset");
+
+    #
+    # If we don't have it in the request, try to find it
+    # in the session
+    #
+    if ( !defined $dataset ) {
+        $dataset = $self->ses->get("dataset");
+    }
+
+    #
+    # If we don't have it in the session, choose the
+    # first one we had read from disk (or cache).
+    #
+    if ( !defined $dataset ) {
+        $dataset = $datasets->[0];
+        $self->ses->set( "dataset", $dataset );
+    }
+
+    $self->dataset($dataset);
+
+    #
+    # Now we should have both $self->dataset, with the
+    # current dataset, and also $self->datasets, with
+    # all data sets, as members.
+    #
+}
+
+
+sub describe {
+    my ( $self, $location ) = @_;
+
+    my $description = $location;
+    $description =~ s/_/ /g;
+    $description = t_case($description);
+
+    return $description;
+}
+
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
