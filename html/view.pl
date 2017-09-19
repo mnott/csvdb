a9a0eebba5023272f533c78eb320d353652c0909
Matthias Nott <matthias.nott@sap.com>
2017-09-19 09:48:46 +0200

    Create a more abstract html app.
    Definitions read from json file rather than having mostly similar
    scripts for each view.
    
    Signed-off-by: Matthias Nott <matthias.nott@sap.com>

---
+++ html/view.pl
@@ -0,0 +1,28 @@
+###################################################
+#
+# Output a View
+#
+# (c) 2017 Matthias Nott
+#
+###################################################
+
+use strict;
+use warnings;
+no warnings qw(redefine);
+
+binmode STDOUT, ":utf8";
+use utf8;
+
+use CSVdb::THTML;
+
+###################################################
+#
+# Run Query
+#
+###################################################
+
+my $req = shift;
+
+my $thtml = CSVdb::THTML->new( req => $req, );
+
+$thtml->run;
