a15e0ba22cb9aa24576ce403c73cfdfd90723bfb
Matthias Nott <matthias.nott@sap.com>
2018-09-05 00:14:31 +0200

    Add non-interactive provision to vagrant

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -6,7 +6,9 @@ sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
 sudo dpkg-reconfigure debconf -f noninteractive -p critical
 
 sudo apt-get update
-sudo apt-get -y upgrade
+# sudo apt-get -y upgrade
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 echo "Custom installations..."
 
