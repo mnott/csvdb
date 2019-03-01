0d07328f5df332d8710e96b616b9fa18206dc1f1
Matthias Nott <matthias.nott@sap.com>
2019-03-01 18:30:32 +0100

    Shorten cpan runtime
    Signed-off-by: Matthias Nott <matthias.nott@sap.com>

--- install/src/configure/configure_perl.sh
+++ install/src/configure/configure_perl.sh
@@ -28,33 +28,33 @@ sudo curl -L https://cpanmin.us 2>/dev/null | perl - App::cpanminus
 if [ -f ../ports ]; then
   sudo cp ../ports /usr/local/bin
   sudo chmod 755 /usr/local/bin/ports
-  sudo cpanm Proc::ProcessTable
+  sudo cpanm --notest Proc::ProcessTable
 fi
 
 # Install a whole bunch of Perl packages
-sudo cpanm Data::Dump
-sudo cpanm Text::Table
-sudo cpanm Text::CSV
-sudo cpanm Tie::IxHash
-sudo cpanm DBI
-sudo cpanm File::BOM
-sudo cpanm DBD::CSV
-sudo cpanm Pod::Markdown
-sudo cpanm Config::Simple
-sudo cpanm Moose
-sudo cpanm Log::Log4perl
-sudo cpanm namespace::autoclean
-sudo cpanm XML::LibXML
-sudo cpanm XML::LibXML::PrettyPrint
-sudo cpanm MooseX::Log::Log4perl
-sudo cpanm Log::Dispatch::Screen
-sudo cpanm URL::Encode
-sudo cpanm Apache::Session::File
-sudo cpanm Cache::Memcached
-sudo cpanm Digest::MD5
-sudo cpanm JSON
-sudo cpanm File::Slurp
-sudo cpanm DateTime::Format::Strptime
+sudo cpanm --notest Data::Dump
+sudo cpanm --notest Text::Table
+sudo cpanm --notest Text::CSV
+sudo cpanm --notest Tie::IxHash
+sudo cpanm --notest DBI
+sudo cpanm --notest File::BOM
+sudo cpanm --notest DBD::CSV
+sudo cpanm --notest Pod::Markdown
+sudo cpanm --notest Config::Simple
+sudo cpanm --notest Moose
+sudo cpanm --notest Log::Log4perl
+sudo cpanm --notest namespace::autoclean
+sudo cpanm --notest XML::LibXML
+sudo cpanm --notest XML::LibXML::PrettyPrint
+sudo cpanm --notest MooseX::Log::Log4perl
+sudo cpanm --notest Log::Dispatch::Screen
+sudo cpanm --notest URL::Encode
+sudo cpanm --notest Apache::Session::File
+sudo cpanm --notest Cache::Memcached
+sudo cpanm --notest Digest::MD5
+sudo cpanm --notest JSON
+sudo cpanm --notest File::Slurp
+sudo cpanm --notest DateTime::Format::Strptime
 
 # Update to the current version of cvsdb to fix BOM error
 cd /tmp
